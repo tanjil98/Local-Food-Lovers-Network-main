39a1332851d5dfce7cb4898403de9ac6c6bedefe
Tanjil Hasan Sakib <98sakib@gmail.com>
2025-11-17 12:02:39 +0600

    update
    update

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_APIKEY,
-    authDomain: import.meta.env.VITE_AUTHDOMAIN,
-    projectId: import.meta.env.VITE_PROJECTID,
-    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-    appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyA5cozvdBUqjVrTj1kovfbGmFk-F3IBCyQ",
+  authDomain: "food-lover-d4878.firebaseapp.com",
+  projectId: "food-lover-d4878",
+  storageBucket: "food-lover-d4878.firebasestorage.app",
+  messagingSenderId: "990366895890",
+  appId: "1:990366895890:web:d17999db1e122b9b0c2dcb",
+  measurementId: "G-PEHHCVZSRQ"
 };
 
 // Initialize Firebase
